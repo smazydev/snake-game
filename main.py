66b911c23d95fe42637bf9dfa3d45e6ac97caad2
smazydev <alizafar1437@gmail.com>
2020-12-25 20:24:14 +0500

    clean implementation of screen and turtle

--- main.py
+++ main.py
@@ -5,15 +5,12 @@ screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
-segment_1 = Turtle("square")
-segment_1.color("white")
+positions = [(0,0),(-20,0),(-40,0)]
 
-segment_2 = Turtle("square")
-segment_2.color("white")
-segment_2.goto(-20,0)
+for position in positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
 
-segment_3 = Turtle("square")
-segment_3.color("white")
-segment_3.goto(-40,0)
 
 screen.exitonclick()
