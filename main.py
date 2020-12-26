0b20f59a09c9b246ba0c4649e9707472f07fae06
smazydev <alizafar1437@gmail.com>
2020-12-26 17:03:46 +0500

    added scoreboard class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -12,6 +13,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
@@ -29,6 +31,7 @@ while game_is_on:
     #Detect collision with Food.
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
     
 
 screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+TEXT_ALIGN = "center"
+TEXT_FONT = ("Courier",24,"normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0,270)
+        self.hideturtle()
+        self.update_scoreboard()
+        
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=TEXT_ALIGN, font=TEXT_FONT)
+
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
