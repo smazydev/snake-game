7f4244c12595db65b4fac8104cc639ce60d38774
smazydev <alizafar1437@gmail.com>
2020-12-27 18:37:52 +0500

    added a file to save highscore

--- snake.py
+++ snake.py
@@ -7,9 +7,6 @@ DOWN = 270
 LEFT = 180
 RIGHT = 0
 
-
-
-
 class Snake:
 
     def __init__(self):
@@ -56,4 +53,12 @@ class Snake:
     
     def right(self):
         if self.head.heading() != LEFT:
-            self.head.setheading(RIGHT)    
+            self.head.setheading(RIGHT)
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000,1000)
+
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
